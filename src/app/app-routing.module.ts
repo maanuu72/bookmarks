47db36d895936e0a64a3709b920dfaa4b34d6ad2
maanuu72 <boligrafoazultortilla94>
2020-07-02 21:23:24 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {BookmarkListComponent} from './pages/bookmark-list/bookmark-list.component';
+import {BookmarkFormComponent} from './pages/bookmark-form/bookmark-form.component';
+import {BookmarkTableComponent} from './pages/bookmark-table/bookmark-table.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BookmarkListComponent
+  },
+  {
+    path: 'table',
+    component: BookmarkTableComponent
+  },
+  {
+    path: 'form',
+    component: BookmarkFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
