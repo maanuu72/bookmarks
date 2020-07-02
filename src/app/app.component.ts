63f13d07928880468bff8f6186eae9247cffbef8
maanuu72 <boligrafoazultortilla94>
2020-07-02 21:23:44 +0200

    bookmark store initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BookmarkStoreService} from './core/store/bookmark/bookmark.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookmarks';
+
+  info = [
+    {name: 'Facebook', url: 'https://www.facebook.com/', group: 'Social'},
+    {name: 'Twitter', url: 'https://twitter.com/explore', group: 'Social'},
+    {name: 'DGT', url: 'http://www.dgt.es/es/', group: 'Gobernment'},
+    {name: 'Ministerio de Interior', url: 'http://www.interior.gob.es/', group: 'Gobernment'},
+    {name: 'Jet Brains', url: 'https://www.jetbrains.com/', group: 'Tools'},
+    {name: 'Store devtools', url: 'https://ngrx.io/guide/store-devtools', group: 'Tools'}
+  ];
+
+  constructor(private bookmarkStoreService: BookmarkStoreService) {
+    this.info.forEach(bookmark => this.bookmarkStoreService.addBookmark(bookmark));
+  }
 }
