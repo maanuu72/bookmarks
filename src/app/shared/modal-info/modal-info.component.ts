b7bc31a3ccf0d73434359b0a626f194cf91724cc
maanuu72 <boligrafoazultortilla94>
2020-07-02 21:25:06 +0200

    dialog

---
+++ src/app/shared/modal-info/modal-info.component.ts
@@ -0,0 +1,22 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal-info',
+  templateUrl: './modal-info.component.html',
+  styleUrls: ['./modal-info.component.css']
+})
+export class ModalInfoComponent {
+
+  constructor(public dialogRef: MatDialogRef<ModalInfoComponent>, @Inject(MAT_DIALOG_DATA) public data) {
+  }
+
+  confirm(): void {
+    this.dialogRef.close(this.data);
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+}
